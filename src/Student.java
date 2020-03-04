6af796a8e6c785f1226211aa61345f80d9a32d78
Iryna Kalanchuk <irynakalanchuk@Irynas-MacBook-Pro.local>
2020-03-05 01:52:04 +0200

    added homework about student's rating

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
-  int rating;
+  private int rating;
   private String name;
+  static Student s1, s2, s3;
+  static double averageRating;
 
   // TODO implement Student class according to the instructions provided in the README.md file
 
   public Student(String name) {
     //TODO initialize name
+    this.name = name;
+  }
+
+  public Student() {
+
   }
 
   public static double getAvgRating() {
     // TODO return average rating of all students
-    return 0;
+    return averageRating;
   }
 
   public String getName() {
@@ -19,6 +29,7 @@ public class Student {
 
   public void setName(String name) {
     // TODO set student's name
+    this.name = name;
   }
 
   public int getRating() {
@@ -27,14 +38,18 @@ public class Student {
 
   public void setRating(int rating) {
     // TODO initialize rating;
+    this.rating = rating;
   }
 
   public boolean betterStudent(Student student) {
     // TODO return the result of comparing this.student's rating with the student's rating
-    return false;
+    return rating >= student.getRating();
   }
 
   public void changeRating(int rating) {
+    this.rating = rating;
+    int sumRating = s1.getRating() + s2.getRating() + s3.getRating();
+    averageRating = ((double)sumRating)/3;
     // TODO change this student's rating and average rating of all students
   }
 
@@ -45,6 +60,41 @@ public class Student {
   @Override
   public String toString() {
     // TODO return String with name and rating of this student
-    return "";
+    return name+rating;
+  }
+
+  public static void main (String[] args) {
+    s1 = new Student();
+    s2 = new Student();
+    s3 = new Student();
+
+    s1.setName("Iryna");
+    s2.setName("Olena");
+    s3.setName("Vitaliy");
+
+    s1.setRating(10);
+    s2.setRating(20);
+    s3.setRating(12);
+
+    int sumRating = s1.getRating() + s2.getRating() + s3.getRating();
+    averageRating = ((double)sumRating)/3;
+
+    System.out.println(getAvgRating());
+
+    if (s1.betterStudent(s2)) {
+      System.out.println(s1.getName() + " is the better student than " + s2.getName());
+    } else {
+      System.out.println(s2.getName() + " is the better student than " + s1.getName());
+    }
+
+    s1.changeRating(22);
+
+    System.out.println(getAvgRating());
+
+    if (s1.betterStudent(s2)) {
+      System.out.println(s1.getName() + " is the better student than " + s2.getName());
+    } else {
+      System.out.println(s2.getName() + " is the better student than " + s1.getName());
+    }
   }
 }
